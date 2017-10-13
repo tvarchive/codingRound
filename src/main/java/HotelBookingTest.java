ce5a795a4d280b67f51f22bcfe2ace78d66ec7e4
Sukesh Kumar <sukesh@testvagrant.com>
2017-10-13 13:04:33 +0530

    changes to hotel booking test

--- src/main/java/HotelBookingTest.java
+++ src/main/java/HotelBookingTest.java
@@ -1,8 +1,8 @@
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
 
 public class HotelBookingTest {
@@ -15,13 +15,25 @@ public class HotelBookingTest {
     @FindBy(id = "Tags")
     private WebElement localityTextBox;
 
+    @FindBy(id = "SearchHotelsButton")
+    private WebElement searchButton;
+
+    @FindBy(id = "travellersOnhome")
+    private WebElement travellerSelection;
+
     @Test
     public void shouldBeAbleToSearchForHotels() {
         System.setProperty("webdriver.chrome.driver", "chromedriver");
 
         driver.get("https://www.cleartrip.com/");
         hotelLink.click();
-        localityTextBox.sendKeys("Bangalore");
+
+        localityTextBox.sendKeys("Indiranagar, Bangalore");
+
+        new Select(travellerSelection).selectByVisibleText("1 room, 2 adults");
+        searchButton.click();
+
+        driver.quit();
 
     }
 
