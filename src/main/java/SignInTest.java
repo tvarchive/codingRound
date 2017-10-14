eed33d654901f50b3de90b06cfb1c0e7b0da9306
Sukesh Kumar <sukesh@testvagrant.com>
2017-10-14 18:58:34 +0530

    addign a pom file for folks who want to go maven way

--- src/main/java/SignInTest.java
+++ src/main/java/SignInTest.java
@@ -1,14 +1,9 @@
 import com.sun.javafx.PlatformUtil;
 import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
-import java.util.List;
 
 public class SignInTest {
 
