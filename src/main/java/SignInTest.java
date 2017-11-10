6ee46fdeff77a89fff8ca5683d4d4af742ad3ef4
Bhareth Prasad <17347@en-bharethp.mynt.myntra.com>
2017-11-10 19:16:05 +0530

    fixed errors in signinTest &added fuction for wait

--- src/main/java/SignInTest.java
+++ src/main/java/SignInTest.java
@@ -1,16 +1,22 @@
 import com.sun.javafx.PlatformUtil;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import static org.openqa.selenium.remote.ErrorCodes.TIMEOUT;
 
 public class SignInTest {
 
     WebDriver driver = new ChromeDriver();
+    private WebDriverWait wait;
 
     @Test
-    public void shouldThrowAnErrorIfSignInDetailsAreMissing() {
+    public void shouldThrowAnErrorIfSignInDetailsAreMissing() throws Exception {
 
         setDriverPath();
 
@@ -19,9 +25,11 @@ public class SignInTest {
 
         driver.findElement(By.linkText("Your trips")).click();
         driver.findElement(By.id("SignIn")).click();
-
+        //switching from default to frame (modal_window)
+        //incase we want to get back to default screen , do switch to default(switchTo().defaultContent())
+        driver.switchTo().frame("modal_window");
+        waitForElementToBeVisible("signInButton","id");
         driver.findElement(By.id("signInButton")).click();
-
         String errors1 = driver.findElement(By.id("errors1")).getText();
         Assert.assertTrue(errors1.contains("There were errors in your submission"));
         driver.quit();
@@ -47,5 +55,34 @@ public class SignInTest {
         }
     }
 
+    /**
+     * this function wail wait for the locator to be visible for 40 sec max.WebDriverWait will call ExpectedCondition every 500 milliseconds until it returns successfully
+     * @param element
+     * @param locator
+     * @return
+     */
 
+    public boolean waitForElementToBeVisible( String element, String locator)
+    {
+        try
+        {
+            wait  = new WebDriverWait(driver, 40);
+            if (locator.equalsIgnoreCase("id"))
+                wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(By.id(element))));
+            else if(locator.equalsIgnoreCase("xpath"))
+                wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(By.xpath(element))));
+            else if(locator.equalsIgnoreCase("className"))
+                wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(By.className(element))));
+            else if(locator.equalsIgnoreCase("cssSelector"))
+                wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(By.cssSelector(element))));
+
+
+        }
+        catch (Exception e)
+        {
+            return false;
+        }
+
+        return true;
+    }
 }
