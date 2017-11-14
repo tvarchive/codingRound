7cbdee3898df7b0162ea05c3d428c844800d140e
nshanmugamk <naveen.k.shanmugam@gmail.com>
2017-11-14 14:59:29 +0530

    changed default package name to com.codetest.tests

--- src/main/java/com/codetest/tests/HotelBookingTest.java
+++ src/main/java/com/codetest/tests/HotelBookingTest.java
@@ -1,3 +1,5 @@
+package com.codetest.tests;
+
 import com.sun.javafx.PlatformUtil;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
--- src/main/java/com/codetest/tests/SignInTest.java
+++ src/main/java/com/codetest/tests/SignInTest.java
@@ -1,3 +1,5 @@
+package com.codetest.tests;
+
 import com.sun.javafx.PlatformUtil;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
