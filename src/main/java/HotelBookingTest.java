b7c2b938f114cdb0a5502ff725dd5bbfa56d9883
Sreenivasareddy10406 <33954717+Sreenivasareddy10406@users.noreply.github.com>
2017-11-24 16:58:24 +0530

    updated HotelBookingTest

--- src/main/java/HotelBookingTest.java
+++ src/main/java/HotelBookingTest.java
@@ -7,9 +7,7 @@ import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
 
 public class HotelBookingTest {
-
-    WebDriver driver = new ChromeDriver();
-
+   // static WebDriver driver;
     @FindBy(linkText = "Hotels")
     private WebElement hotelLink;
 
@@ -24,30 +22,32 @@ public class HotelBookingTest {
 
     @Test
     public void shouldBeAbleToSearchForHotels() {
-        setDriverPath();
+     //   setDriverPath();
+    //    driver=new ChromeDriver();
 
         driver.get("https://www.cleartrip.com/");
-        hotelLink.click();
+        clickOnElement(hotelLink);
 
         localityTextBox.sendKeys("Indiranagar, Bangalore");
 
-        new Select(travellerSelection).selectByVisibleText("1 room, 2 adults");
-        searchButton.click();
+        Select select=new Select(travellerSelections)
+        select.selectByVisibleText("1 room, 2 adults");
+        clickOnElement(searchButton);
 
         driver.quit();
 
     }
 
-    private void setDriverPath() {
-        if (PlatformUtil.isMac()) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver");
-        }
-        if (PlatformUtil.isWindows()) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-        }
-        if (PlatformUtil.isLinux()) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver_linux");
-        }
-    }
+//     private void setDriverPath() {
+//         if (PlatformUtil.isMac()) {
+//             System.setProperty("webdriver.chrome.driver", "chromedriver");
+//         }
+//         if (PlatformUtil.isWindows()) {
+//             System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+//         }
+//         if (PlatformUtil.isLinux()) {
+//             System.setProperty("webdriver.chrome.driver", "chromedriver_linux");
+//         }
+//     }
 
 }
