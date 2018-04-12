9355eff16df241590441da840262c673f5a88ddd
iAbhiChops <chopra.abhishek93@gmail.com>
2018-04-12 18:24:23 +0530

    Update HotelBookingTest.java

--- src/main/java/HotelBookingTest.java
+++ src/main/java/HotelBookingTest.java
@@ -1,16 +1,14 @@
-import com.sun.javafx.PlatformUtil;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.Select;
-import org.testng.annotations.Test;
 
-public class HotelBookingTest {
 
-    WebDriver driver = new ChromeDriver();
+public class HotelBookingPage {
+    WebDriver driver;
 
-    @FindBy(linkText = "Hotels")
+    @FindBy(xpath = ".//a[contains(@title,'Find hotels')]")
     private WebElement hotelLink;
 
     @FindBy(id = "Tags")
@@ -22,32 +20,21 @@ public class HotelBookingTest {
     @FindBy(id = "travellersOnhome")
     private WebElement travellerSelection;
 
-    @Test
-    public void shouldBeAbleToSearchForHotels() {
-        setDriverPath();
+    public void HotelBookingPage(WebDriver driver){
+        this.driver = driver;
+    }
 
-        driver.get("https://www.cleartrip.com/");
+    public void shouldBeAbleToSearchForHotels() {
+
+        PageFactory.initElements(driver, this);
         hotelLink.click();
+
+        PageFactory.initElements(driver, this);
+        localityTextBox.click();
 
         localityTextBox.sendKeys("Indiranagar, Bangalore");
 
         new Select(travellerSelection).selectByVisibleText("1 room, 2 adults");
         searchButton.click();
-
-        driver.quit();
-
     }
-
-    private void setDriverPath() {
-        if (PlatformUtil.isMac()) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver");
-        }
-        if (PlatformUtil.isWindows()) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-        }
-        if (PlatformUtil.isLinux()) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver_linux");
-        }
-    }
-
 }
