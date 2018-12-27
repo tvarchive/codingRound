d891f2cac5ea639f3e10750b840362eae0496090
ItsKaranzzz <karanchaudhary70@gmail.com>
2018-12-27 10:41:56 +0530

    Removed the static wait and updated with the successful execution of Sign In test case

--- src/main/java/SignInTest.java
+++ src/main/java/SignInTest.java
@@ -3,6 +3,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -11,21 +13,27 @@ import com.sun.jna.Platform;
 public class SignInTest {
 
 	WebDriver driver;
+	ChromeOptions options;
+	WebDriverWait oWait;
 
 	@Test
 	public void shouldThrowAnErrorIfSignInDetailsAreMissing() {
-
+		options = new ChromeOptions();
+		options.addArguments("--disable-notifications");
 		setDriverPath();
-		driver = new ChromeDriver();
+		driver = new ChromeDriver(options);
 
 		driver.get("https://www.cleartrip.com/");
-		waitFor(2000);
+
+		oWait = new WebDriverWait(driver, 1000);
+		oWait.until(ExpectedConditions.visibilityOf(driver.findElement(By.linkText("Your trips"))));
 
 		driver.findElement(By.linkText("Your trips")).click();
 		driver.findElement(By.id("SignIn")).click();
-		driver.switchTo().frame("modal_window");
-		driver.findElement(By.id("signInButton")).click();
+		driver.switchTo().frame(1);
 
+		driver.findElement(By.id("signInButton")).click();
+		oWait.until(ExpectedConditions.visibilityOf(driver.findElement(By.id("errors1"))));
 		String errors1 = driver.findElement(By.id("errors1")).getText();
 		Assert.assertTrue(errors1.contains("There were errors in your submission"));
 		driver.quit();
