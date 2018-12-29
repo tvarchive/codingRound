b63c4c39d2cdb6fee7b7d43f860dfedc467f0279
ItsKaranzzz <karanchaudhary70@gmail.com>
2018-12-29 13:23:49 +0530

    updated Flight Booking TC in sync with POM framework design

--- src/main/java/FlightBookingTest.java
+++ src/main/java/FlightBookingTest.java
@@ -1,51 +1,31 @@
 
-import java.util.List;
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import pages.FlightBookingPage;
 import testbase.TestBase;
-import utils.CommonFunctions;
+
+import utils.Constants;
 
 public class FlightBookingTest extends TestBase {
+	FlightBookingPage oFlighBooking;
 
 	@Test
 	public void testThatResultsAppearForAOneWayJourney() {
 
-		WebElement oneway = driver.findElement(By.id("OneWay"));
-		CommonFunctions.explicitWaitForElement(oneway, 50, driver);
+		oFlighBooking = new FlightBookingPage();
 
-		CommonFunctions.clickOnElement(oneway);
+		oFlighBooking.clickOnOneWayLink();
 
-		WebElement fromtag = driver.findElement(By.id("FromTag"));
-		CommonFunctions.enterOntoElement(fromtag, "Bangalore");
+		oFlighBooking.enterAndSelectFromLocation(Constants.FROM_CITY);
 
-		By locatorofLoadedFromCity = By.xpath("//ul[@id='ui-id-1']/li/a");
+		oFlighBooking.enterAndSelectToLocation(Constants.TO_CITY);
 
-		// wait for the auto complete options to appear for the origin
-		CommonFunctions.fluentWaitForelementLocator(locatorofLoadedFromCity, 200, 2, driver);
+		oFlighBooking.selectFromDate(Constants.FROM_DATE_IN_DDMMYYYY);
 
-		List<WebElement> originOptions = driver.findElement(By.id("ui-id-1")).findElements(By.tagName("li"));
-		originOptions.get(0).click();
+		oFlighBooking.clickOnSearchButton();
 
-		WebElement totag = driver.findElement(By.id("ToTag"));
-		CommonFunctions.enterOntoElement(totag, "Delhi");
-
-		By locatorofLoadedDestinationCity = By.xpath("//ul[@id='ui-id-2']/li/a");
-		// wait for the auto complete options to appear for the destination
-		CommonFunctions.fluentWaitForelementLocator(locatorofLoadedDestinationCity, 200, 2, driver);
-		// select the first item from the destination auto complete list
-		List<WebElement> destinationOptions = driver.findElement(By.id("ui-id-2")).findElements(By.tagName("li"));
-		destinationOptions.get(0).click();
-
-		// selecting date on the calendar
-		CommonFunctions.calendarHandling("02/03/2019", driver);
-		// all fields filled in. Now click on search
-		driver.findElement(By.id("SearchBtn")).click();
-
-		Assert.assertTrue(CommonFunctions.isElementPresent(By.className("searchSummary"), driver));
+		Assert.assertEquals((oFlighBooking.checkForSearchSummary()), true);
 
 	}
 
--- src/main/java/pages/FlightBookingPage.java
+++ src/main/java/pages/FlightBookingPage.java
@@ -1,5 +1,84 @@
 package pages;
 
-public class FlightBookingPage {
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import testbase.TestBase;
+import utils.CommonFunctions;
+import utils.Constants;
+
+public class FlightBookingPage extends TestBase {
+
+	@FindBy(id = "OneWay")
+	private WebElement OneWayLink;
+
+	@FindBy(id = "FromTag")
+	private WebElement FromLocation;
+
+	@FindBy(id = "ui-id-1")
+	private WebElement fromDropDown;
+
+	@FindBy(id = "ui-id-2")
+	private WebElement ToDropDown;
+
+	@FindBy(id = "ToTag")
+	private WebElement ToLocation;
+
+	@FindBy(id = "SearchBtn")
+	private WebElement SearchBtn;
+
+	private By fromOrToDropDownValues = By.tagName("li");
+	private By searchSummary = By.className("searchSummary");
+	private By fromDropDownValue = By.xpath("//ul[@id='ui-id-1']/descendant::a");
+	private By ToDropDownValue = By.xpath("//ul[@id='ui-id-2']/descendant::a");
+
+	public FlightBookingPage() {
+		PageFactory.initElements(driver, this);
+	}
+
+	public void clickOnOneWayLink() {
+		CommonFunctions.explicitWaitForElement(OneWayLink, Constants.EXPLICIT_WAIT, driver);
+		CommonFunctions.clickOnElement(OneWayLink);
+	}
+
+	public void enterAndSelectFromLocation(String fromCity) {
+
+		CommonFunctions.enterOntoElement(FromLocation, fromCity);
+		CommonFunctions.fluentWaitForelementLocator(fromDropDownValue, Constants.FLUENT_WAIT_TIMEOUT,
+				Constants.FLUENT_WAIT_POLLING_FQ, driver);
+
+		List<WebElement> originOptions = fromDropDown.findElements(fromOrToDropDownValues);
+		CommonFunctions.clickOnElement(originOptions.get(0));
+
+	}
+
+	public void enterAndSelectToLocation(String ToCity) {
+
+		CommonFunctions.enterOntoElement(ToLocation, ToCity);
+		CommonFunctions.fluentWaitForelementLocator(ToDropDownValue, Constants.FLUENT_WAIT_TIMEOUT,
+				Constants.FLUENT_WAIT_POLLING_FQ, driver);
+
+		List<WebElement> originOptions = ToDropDown.findElements(fromOrToDropDownValues);
+		CommonFunctions.clickOnElement(originOptions.get(0));
+
+	}
+
+	public void selectFromDate(String date) {
+
+		CommonFunctions.calendarHandling(date, driver);
+	}
+
+	public void clickOnSearchButton() {
+		CommonFunctions.clickOnElement(SearchBtn);
+	}
+
+	public boolean checkForSearchSummary() {
+
+		return CommonFunctions.isElementPresent(searchSummary, driver);
+	}
 
 }
