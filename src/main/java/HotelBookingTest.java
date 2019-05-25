2b7513f86a7920f264efb676a9b20cf393e42cda
EswarReddyk <51030611+EswarReddyk@users.noreply.github.com>
2019-05-26 00:30:58 +0530

    Locations and Travel sellction Using Through Confing file
    Locations and Travel sellction Using Through Confing file

--- src/main/java/HotelBookingTest.java
+++ src/main/java/HotelBookingTest.java
@@ -1,53 +1,44 @@
 import com.sun.javafx.PlatformUtil;
+
+import Pages.HotelBooking;
+import Testbase.TestBase;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class HotelBookingTest {
+public class HotelBookingTest extends TestBase{
 
-    WebDriver driver = new ChromeDriver();
-
-    @FindBy(linkText = "Hotels")
-    private WebElement hotelLink;
-
-    @FindBy(id = "Tags")
-    private WebElement localityTextBox;
-
-    @FindBy(id = "SearchHotelsButton")
-    private WebElement searchButton;
-
-    @FindBy(id = "travellersOnhome")
-    private WebElement travellerSelection;
+	   
+	HotelBooking  HB ;
+	
+	@BeforeMethod
+	public void setupHomePage()
+	{
+		BrowserUrllaunch();		
+	}
 
     @Test
-    public void shouldBeAbleToSearchForHotels() {
-        setDriverPath();
+    public void shouldBeAbleToSearchForHotels() throws InterruptedException
+    {
 
-        driver.get("https://www.cleartrip.com/");
-        hotelLink.click();
-
-        localityTextBox.sendKeys("Indiranagar, Bangalore");
-
-        new Select(travellerSelection).selectByVisibleText("1 room, 2 adults");
-        searchButton.click();
-
-        driver.quit();
-
+      HB = new HotelBooking();
+      HB.HotelLinkClick();
+      HB.localityTextBox(prop.getProperty("HotelLocationName"));
+      waitFor(9000);
+      HB.travellerSelection(prop.getProperty("TravelSelection"));
     }
 
-    private void setDriverPath() {
-        if (PlatformUtil.isMac()) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver");
-        }
-        if (PlatformUtil.isWindows()) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-        }
-        if (PlatformUtil.isLinux()) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver_linux");
-        }
-    }
-
+     @AfterMethod
+     public void  tearDown()
+     {
+    	driver.quit(); 
+     }
+     
 }
+© 2019 GitHub, Inc.
