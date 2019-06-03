98c1d0b19bd80895e80be567bf3cd83586c0ef7f
Yogita8305 <51274001+Yogita8305@users.noreply.github.com>
2019-06-03 12:54:34 +0530

    UpdatedFlightBookingFinal.java

--- src/main/java/FlightBookingTest.java
+++ src/main/java/FlightBookingTest.java
@@ -13,12 +13,16 @@ import java.util.List;
 public class FlightBookingTest {
 
     WebDriver driver = new ChromeDriver();
+    @BeforeMethod
+    public void beforeMethod(){
+        setDriverPath();
+    }
 
 
     @Test
     public void testThatResultsAppearForAOneWayJourney() {
 
-        setDriverPath();
+        //setDriverPath();
         driver.get("https://www.cleartrip.com/");
         waitFor(2000);
         driver.findElement(By.id("OneWay")).click();
@@ -52,8 +56,13 @@ public class FlightBookingTest {
         Assert.assertTrue(isElementPresent(By.className("searchSummary")));
 
         //close the browser
-        driver.quit();
+       // driver.quit();
 
+    }
+    
+    @AfterMethod
+    public void afterMethod(){
+        driver.quit();
     }
 
 
@@ -79,10 +88,10 @@ public class FlightBookingTest {
         if (PlatformUtil.isMac()) {
             System.setProperty("webdriver.chrome.driver", "chromedriver");
         }
-        if (PlatformUtil.isWindows()) {
+      else  if (PlatformUtil.isWindows()) {
             System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
         }
-        if (PlatformUtil.isLinux()) {
+      else  if (PlatformUtil.isLinux()) {
             System.setProperty("webdriver.chrome.driver", "chromedriver_linux");
         }
     }
