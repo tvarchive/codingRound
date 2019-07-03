d4f05495f26fad859247b6f0eb4f4f03cf044c5e
Ajay Clement <clementajay@gmail.com>
2019-07-03 23:27:18 +0530

    Adding a constant for SignInPage

--- src/main/java/com/codinground/uicommon/UiConstants.java
+++ src/main/java/com/codinground/uicommon/UiConstants.java
@@ -6,5 +6,6 @@ public class UiConstants {
 	public static final String BANGALORE = "BLR";
 	public static final String NEW_DELHI = "DEL";
 	public static final String ATTRIBUTE_CHECKED = "true";
+	public static final String SIGN_IN_ERROR = "There were errors in your submission";
 
 }
