1510d916488cc790bac01ad3abd8d0d32f0f862b
Jatin Suresh Borikar <44162179+jsureshborikar@users.noreply.github.com>
2019-08-23 16:04:50 +0530

    Commit-4

--- src/main/java/HotelBookingTest.java
+++ src/main/java/HotelBookingTest.java
@@ -1,6 +1,7 @@
 import com.sun.javafx.PlatformUtil;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -47,6 +48,7 @@ public class HotelBookingTest {
     	setDriverPath();
     	PageFactory.initElements(driver, this);
         
+    	driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     	driver.get("https://www.cleartrip.com/");
         hotelLink.click();
 
--- src/main/java/SignInTest.java
+++ src/main/java/SignInTest.java
@@ -1,14 +1,19 @@
 import com.sun.javafx.PlatformUtil;
+
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class SignInTest {
 
-	 WebDriver driver;
+	WebDriver driver;
 	    
 	@BeforeTest
 	public void driverInitialization() {
@@ -16,23 +21,25 @@ public class SignInTest {
 		driver = new ChromeDriver();
 	}
 	
-	
     @Test
     public void shouldThrowAnErrorIfSignInDetailsAreMissing() {
 
         setDriverPath();
-
+        
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get("https://www.cleartrip.com/");
         waitFor(2000);
 
         driver.findElement(By.linkText("Your trips")).click();
         driver.findElement(By.id("SignIn")).click();
 
+        WebElement frame = driver.findElement(By.id("modal_window"));
+        driver.switchTo().frame(frame);
+        
         driver.findElement(By.id("signInButton")).click();
-
+        waitFor(3000);
         String errors1 = driver.findElement(By.id("errors1")).getText();
         Assert.assertTrue(errors1.contains("There were errors in your submission"));
-        driver.quit();
     }
 
     private void waitFor(int durationInMilliSeconds) {
@@ -55,5 +62,10 @@ public class SignInTest {
         }
     }
 
+    @AfterTest
+    public void quit() {
+    	//close the browser
+        driver.quit();
+    }
 
 }
