ecac9628b35327ddc0917fbcceec12f6e97a693b
Dhanavarsha <dhan.varsha03@gmail.com>
2019-12-23 13:53:06 -0800

    Added chromedriver log and chromeoptions

--- src/main/java/application/App.java
+++ src/main/java/application/App.java
@@ -3,11 +3,15 @@ package application;
 import com.sun.javafx.PlatformUtil;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 public class App {
-    private WebDriver driver;
+    private final WebDriver driver;
     private static App app;
 
     public static App getInstance() {
@@ -17,13 +21,10 @@ public class App {
         return app;
     }
 
-    public App() {
+    private App() {
         setDriverPath();
-        driver = new ChromeDriver();
-    }
-
-    public WebDriver getDriver() {
-        return driver;
+        createLogForChromeDriver();
+        driver = new ChromeDriver(getChromeOptions());
     }
 
     private void setDriverPath() {
@@ -36,6 +37,28 @@ public class App {
         if (PlatformUtil.isLinux()) {
             System.setProperty("webdriver.chrome.driver", "src/main/drivers/chromedriver_linux");
         }
+    }
+
+    private void createLogForChromeDriver() {
+        String currentWorkingDirectory = new File(".").getAbsolutePath();
+        System.out.println("currentWorkingDirectory : "+currentWorkingDirectory);
+        final String chromeDriverLogFilePath = currentWorkingDirectory + File.separator + "chromedriver.log";
+        System.out.println("Logging at:" + chromeDriverLogFilePath);
+        System.setProperty("webdriver.chrome.logfile", chromeDriverLogFilePath);
+        System.setProperty("webdriver.chrome.verboseLogging", "true");
+    }
+
+    private ChromeOptions getChromeOptions() {
+        ChromeOptions options = new ChromeOptions();
+        Map<String, Object> prefs = new HashMap<String, Object>();
+        prefs.put("profile.default_content_setting_values.notifications", 2);
+        options.setExperimentalOption("prefs", prefs);
+        options.addArguments("--start-maximized");
+        return options;
+    }
+
+    public WebDriver getDriver() {
+        return driver;
     }
 
     public void launch() {
