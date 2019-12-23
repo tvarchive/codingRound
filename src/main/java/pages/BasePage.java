dee5fc1ae66de21ca37d701738b6e391380f4a02
Dhanavarsha <dhan.varsha03@gmail.com>
2019-12-23 13:54:07 -0800

    Modified access specifiers

--- src/main/java/pages/BasePage.java
+++ src/main/java/pages/BasePage.java
@@ -19,7 +19,7 @@ public abstract class BasePage {
 
     abstract WebElement uniquePageIdentifier();
 
-    public WebDriver getDriver() {
+    WebDriver getDriver() {
         return driver;
     }
 
@@ -29,23 +29,23 @@ public abstract class BasePage {
         return wait;
     }
 
-    protected WebElement getElement(By by) {
+    WebElement getElement(By by) {
         return driver.findElement(by);
     }
 
-    protected WebDriverWait waitForElementsVisibility(List<WebElement> element, int duration) {
+    WebDriverWait waitForElementsVisibility(List<WebElement> element, int duration) {
         WebDriverWait wait = new WebDriverWait(driver, duration);
         wait.until(ExpectedConditions.visibilityOfAllElements(element));
         return wait;
     }
 
-    protected WebDriverWait waitForElementVisibility(WebElement element, int duration) {
+    WebDriverWait waitForElementVisibility(WebElement element, int duration) {
         WebDriverWait wait = new WebDriverWait(driver, duration);
         wait.until(ExpectedConditions.visibilityOf(element));
         return wait;
     }
 
-    protected boolean isElementPresent(By by) {
+    boolean isElementPresent(By by) {
         try {
             driver.findElement(by);
             return true;
@@ -54,7 +54,7 @@ public abstract class BasePage {
         }
     }
 
-    protected void switchToFrame(WebElement frameElement) {
+    void switchToFrame(WebElement frameElement) {
         try {
             driver.switchTo().frame(frameElement);
         }
