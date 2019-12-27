ad1835f59ad4e57f490babf9ea42a957d976060f
Karthik Penujuru <penujuru.karthik@bcg.com>
2019-12-27 23:29:32 +0530

    1) Updated the windows chrome driver 2) Added Out folder into gitignore 3) Added three new tests in FlightBooking, HotelBooking, SignInTest Class

---
+++ src/main/java/Credentials.java
@@ -0,0 +1,18 @@
+public class Credentials {
+
+    private String username;
+    private String password;
+
+    public Credentials() {
+        username = "test@testing.com";
+        password = "password@123";
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
